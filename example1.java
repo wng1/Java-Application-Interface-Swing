acca28f3ce8427e52dc98a99d959cd5e917aaa0c
wng1 <wng1@users.noreply.github.com>
2016-03-20 20:39:07 +0000

    Update example1.java

--- example1.java
+++ example1.java
@@ -1,2 +1,7 @@
+//1.0
 button.addActionListener(event -> System.out.println("You have submitted your form"));
 
+//Utilising an internal iteration over external iteration   = allOrder.SFC
+long count = allOrders.stream()
+                      .filter(order -> order.isFrom("New York"))
+                      .count();
